75f80fb92ac85dc94055ca3ac89580aedf0ef15c
Patrick Niyogitare <patrickniyogitare28@gmail.com>
2021-01-16 23:53:34 +0200

    bug- fixed reducer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import {Store} from '@ngrx/store';
 import {Observable} from 'rxjs';
+import {Post} from './models/post.model';
+import * as PostActions from './actions/post.action';
 
 interface AppState {
   message: string;
+  post: Post;
 }
 
 @Component({
@@ -12,19 +15,38 @@ interface AppState {
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  message$: Observable<string>
+  message$: Observable<string>;
+  post$: Observable<Post>;
+  text: string='';
   title = 'ngrx-starter';
 
   constructor(private store: Store<AppState>){
     this.message$ = this.store.select('message');
-
+    this.post$ = this.store.select('post');
   }
 
+
+  
   spanishMessage(){
     this.store.dispatch({type:'SPANISH'});
   }
 
   frenchMessage(){
     this.store.dispatch({type: 'FRENCHE'});
+  }
+
+  editText(){
+     this.store.dispatch(new PostActions.EditText(this.text))
+  }
+
+  upvote(){
+     this.store.dispatch(new PostActions.Upvote());
+  }
+
+  reset(){
+     this.store.dispatch(new PostActions.Reset());
+  }
+  downVote(){
+    this.store.dispatch(new PostActions.Downvote());
   }
 }
--- src/app/reducers/post.reducers.ts
+++ src/app/reducers/post.reducers.ts
@@ -1,3 +1,37 @@
-export function postReducer(){
+import * as PostActions from '../actions/post.action';
+import {Post} from '../models/post.model';
+// import {Action} from '@ngrx/store';
+
+
+export type Action = PostActions.All; 
+//default Post state;
+
+const defaultState: Post = {
+    text: 'This is the default state text',
+    likes: 0
+}
+
+// Function to create new state object
+const newState = (state:any, newData:any) => {
+    return Object.assign({}, state, newData);
+}
+
+// Reducer function
+export function postReducer(state: Post = defaultState, action: Action){
+   switch(action.type){
+       case PostActions.EDIT_TEXT:
+        return newState(state, { text: action.payload });
+        
+        case PostActions.UPVOTE: 
+            return newState(state, {likes: state.likes + 1});    
+
+        case PostActions.DOWNVOTE:
+            return newState(state, {likes: state.likes - 1})
+            
+        case PostActions.RESET: 
+            return defaultState;    
+        default:
+            return state;    
+   }
     
 }
